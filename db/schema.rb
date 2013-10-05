82dc25a007923b81d185311ec1038e1c98a00728
Andrew Hausdorf <ajhausdorf@wustl.edu>
2013-10-05 16:37:34 +0200

    Rename Microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130925174139) do
+ActiveRecord::Schema.define(version: 20131005140920) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20130925174139) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
